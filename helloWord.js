58bd62b08aae72ef297ed077de0d3b514fa3a1d4
Minakshi Kadao <minakshikadao41@gmail.com>
2023-12-29 15:20:49 +0530

    swapvariable
    swapvariable

---
+++ helloWord.js
@@ -0,0 +1,14 @@
+
+console.log("Hello JS , I am Minakshi");
+
+var myName; // Varibale Declartion
+console.log(myName); // undefined
+
+myName = "Minakshi"; // Variable Intialization
+console.log("First Name: ",myName);
+
+ var age = "28"; // Varibale declaration and Initialization
+console.log("Age:", age);
+
+ myName = "Minakshi Kadao"; // Varibale update
+console.log("Full name: ", myName);
