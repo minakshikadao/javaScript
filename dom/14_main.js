045768c056377baa0d42586357d1e536217ee16c
Minakshi Kadao <minakshikadao41@gmail.com>
2024-01-25 13:24:34 +0530

    Dom-14_my profile assignment
    Dom-14_my profile assignment

---
+++ dom/14_main.js
@@ -0,0 +1,20 @@
+
+const elementProfileLink = document.querySelector('#profileLink');
+elementProfileLink.setAttribute('href','https://www.linkedin.com/')
+console.log(elementProfileLink);
+
+const elementProfileDetails = document.getElementById("details");
+elementProfileDetails.style.color= 'orange';
+elementProfileDetails.style.fontFamily= 'Arial';
+console.log(elementProfileDetails);
+
+const elementTechStackH2 = document.querySelector('.techStack');
+elementTechStackH2 = `Complete Technology Stack is :`
+console.log(elementTechStackH2);
+
+
+
+
+
+
+
